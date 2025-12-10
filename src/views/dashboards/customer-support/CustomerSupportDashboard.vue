89e874da6138f93b941e64fa1c8a43dd52937967
Jakub Kozler <j.kozler@nubeo.eu>
2025-12-10 11:25:51 +0100

    Add new dashboard for customer support

--- src/views/dashboards/customer-support/CustomerSupportDashboard.vue
+++ src/views/dashboards/customer-support/CustomerSupportDashboard.vue
@@ -16,6 +16,7 @@ onMounted(() => {
         <v-tabs v-model="activeTab" bg-color="primary">
           <v-tab value="0">Chybějící produkty</v-tab>
           <v-tab value="1">Přehled problémů a stavů</v-tab>
+          <v-tab value="2">Data error objednávky</v-tab>
         </v-tabs>
 
         <v-card-text>
@@ -30,6 +31,11 @@ onMounted(() => {
             <v-window-item value="1">
                 <v-card-text style="height: 1200px;">
                   <iframe style="width: 100%;height: 100%;" src="https://grafana.naturalprotein.cz/modules/grafana/service/public-dashboards/ff3e7a62b357416d8c0f0722f5334d97"></iframe>
+                </v-card-text>
+            </v-window-item>
+            <v-window-item value="2">
+                <v-card-text style="height: 1200px;">
+                  <iframe style="width: 100%;height: 100%;" src="https://grafana.naturalprotein.cz/modules/grafana/service/public-dashboards/7e15c1c17e7542268f3e5cd390761afd?orgId=1"></iframe>
                 </v-card-text>
             </v-window-item>
           </v-window>
